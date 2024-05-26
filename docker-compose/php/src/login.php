9af82dc91bde51c16f0bcf82798c408e845b4584
marked_d <p19mark@ionio.gr>
2024-05-27 00:29:13 +0300

    Fixed deprecated

--- docker-compose/php/src/login.php
+++ docker-compose/php/src/login.php
@@ -33,6 +33,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $storedPasswordHash = $row['password'];
 
         if (password_verify($password, $storedPasswordHash)) {
+        $storedPasswordHash = $row['password'];
+
+        if (password_verify($password, $storedPasswordHash)) {
             $_SESSION['user_id'] = $row['id'];
             $_SESSION['username'] = $row['username'];
 
