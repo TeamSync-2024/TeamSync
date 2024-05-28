2f8df83e91f618e19450ddad8d84f91f9e0737ca
marked_d <p19mark@ionio.gr>
2024-05-28 17:17:19 +0300

    Added various styles and scripts

--- docker-compose/php/src/error.php
+++ docker-compose/php/src/error.php
@@ -1,9 +1,15 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>Error</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>TeamSync - Task Management Website</title>
+  <link rel="stylesheet" href="styles.css">
+  <script src="script.js" defer></script>
 </head>
 <body>
+    <div id="header-container"></div>
+    <main>
     <h1>Error</h1>
     <?php
     if (isset($_GET['error'])) {
@@ -12,5 +18,7 @@
         echo "<p>An unknown error occurred.</p>";
     }
     ?>
+    </main>
+    <div id="footer-container"></div>
 </body>
 </html>
