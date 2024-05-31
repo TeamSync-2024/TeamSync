547ae2b0574e693e1bbd719015055233042c8482
marked_d <p19mark@ionio.gr>
2024-05-31 20:06:18 +0300

    update

--- docker-compose/php/src/config.php
+++ docker-compose/php/src/config.php
@@ -4,4 +4,5 @@ define('DB_HOST', getenv('DB_HOST') ?: 'mysql');
 define('DB_USER', getenv('DB_USER') ?: 'webuser');
 define('DB_PASS', getenv('DB_PASS') ?: 'webpass');
 define('DB_NAME', getenv('DB_NAME') ?: 'di_internet_technologies_project');
+
 ?>
